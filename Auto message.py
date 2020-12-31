4f80c0cd63d21959548d813dc1e2d8e3617b0d53
Jun <73165472+97me@users.noreply.github.com>
2020-12-31 15:25:08 +0900

    Update Auto message.py

--- Auto message.py
+++ Auto message.py
@@ -10,6 +10,3 @@ driver.find_element_by_xpath('//*[@id="btnlogin"]').click()
 driver.find_element_by_xpath('/html/body/div/form/div/div[2]/div[1]/section[1]/div[3]/input[1]').click()
 
 driver.find_element_by_name('message').send_keys('message message message')
-
-# ○○만든이 : 성실한 준혁
-# ○○사용법: 프로젝트-> 디버깅하지 않고 시작 -> 기다리면 됩니다^^ 
